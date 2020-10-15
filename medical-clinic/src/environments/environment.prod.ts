8cbb990cef88b12ac7f017a0d3686a21c5175ab6
Carlos Mori <cmori.94@gmail.com>
2020-10-15 20:43:00 -0300

    feat: set firebase hosting, eslint, prettier, ngPrime

---
+++ medical-clinic/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCY3pft9e1j9r-Z49BF4SIhkWim7RrMHto',
+    authDomain: 'medical-clinic-c44ab.firebaseapp.com',
+    databaseURL: 'https://medical-clinic-c44ab.firebaseio.com',
+    projectId: 'medical-clinic-c44ab',
+    storageBucket: 'medical-clinic-c44ab.appspot.com',
+    messagingSenderId: '641631737346',
+    appId: '1:641631737346:web:ed9497221a20f45bb8b91b',
+    measurementId: 'G-2LG82SHFBL',
+  },
+};
---
+++ medical-clinic/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCY3pft9e1j9r-Z49BF4SIhkWim7RrMHto',
+    authDomain: 'medical-clinic-c44ab.firebaseapp.com',
+    databaseURL: 'https://medical-clinic-c44ab.firebaseio.com',
+    projectId: 'medical-clinic-c44ab',
+    storageBucket: 'medical-clinic-c44ab.appspot.com',
+    messagingSenderId: '641631737346',
+    appId: '1:641631737346:web:ed9497221a20f45bb8b91b',
+    measurementId: 'G-2LG82SHFBL',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
