c49d6b4ce4458e5ef81d59f48144808566d12fad
Carlos Mori <cmori.94@gmail.com>
2020-10-18 20:28:00 -0300

    feat: partial implementation of registration process, full layout building

--- medical-clinic/src/app/components/login/login.component.ts
+++ medical-clinic/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  // Login Models
   username;
   password;
-  constructor() {}
+  // Registration Models
+  name;
+  userEmail;
+  repeatUserEmail;
+  userPassword;
+  repeatUserPassword;
+
+  displayRegisterForm: boolean;
+  registrationMessage: string;
+  displayMessage: boolean;
+  newUser: User;
+  constructor() {
+    this.displayRegisterForm = true;
+    this.displayMessage = false;
+  }
 
   ngOnInit(): void {}
+
+  switchForms() {
+    this.displayRegisterForm = !this.displayRegisterForm;
+  }
+  register() {
+    // TODO uncoment this when done
+    // this.displayMessage = true;
+    // this.registrationMessage = 'Thanks for Registering, please check your email';
+    this.newUser = { name: this.name, email: this.userEmail, password: this.userPassword };
+    console.log(this.newUser);
+  }
 }
--- medical-clinic/src/app/services/auth.service.ts
+++ medical-clinic/src/app/services/auth.service.ts
@@ -13,33 +13,59 @@ export class AuthService {
     public ngZone: NgZone
   ) {}
 
-  
-  SignIn(email, password) {
+  SignUp(email: string, password: string) {
     return this.afAuth
-      .signInWithEmailAndPassword(email, password)
-      .then(() => {
-        this.ngZone.run(() => {
-          this.afAuth.currentUser.then((result) => {
-            const { uid, displayName, photoURL, email } = result;
-            this.userData = {
-              uid,
-              displayName,
-              photoURL,
-              email
-            };
-            this.isLoggedIn = true;
-            localStorage.setItem('user', JSON.stringify(this.userData));
-            this.router.navigate(['/Principal']);
-          });
-        });
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        /* Call the SendVerificaitonMail() function when new user sign 
+      up and returns promise */
+        this.SendVerificationMail(email);
+        // this.SetUserData(result.user);
       })
       .catch((error) => {
-        console.log(error);
-        console.log('Error in Login login');
+        window.alert(error.message);
       });
   }
+  SendVerificationMail(email) {
+    // TODO continue checking https://firebase.google.com/docs/auth/web/email-link-auth?hl=es
+    return this.afAuth
+      .sendSignInLinkToEmail(email, null)
+      .then(function () {
+        // The link was successfully sent. Inform the user.
+        // Save the email locally so you don't need to ask the user for it again
+        // if they open the link on the same device.
+        window.localStorage.setItem('emailForSignIn', email);
+      })
+      .catch(function (error) {
+        // Some error occurred, you can inspect the code: error.code
+      });
+  }
+  // SignIn(email, password) {
+  //   return this.afAuth
+  //     .signInWithEmailAndPassword(email, password)
+  //     .then(() => {
+  //       this.ngZone.run(() => {
+  //         this.afAuth.currentUser.then((result) => {
+  //           const { uid, displayName, photoURL, email } = result;
+  //           this.userData = {
+  //             uid,
+  //             displayName,
+  //             photoURL,
+  //             email
+  //           };
+  //           this.isLoggedIn = true;
+  //           localStorage.setItem('user', JSON.stringify(this.userData));
+  //           this.router.navigate(['/Principal']);
+  //         });
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       console.log('Error in Login login');
+  //     });
+  // }
   Logout() {
     localStorage.removeItem('user');
-    this.router.navigate(['/Login']);
+    this.router.navigate(['/']);
   }
 }
